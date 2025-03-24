3ecbc8e7039a4bf6d48903b75f2bd6ff5db4a2c0
ManaR-Rch <manarmarchou6@gmail.com>
2025-03-24 15:29:30 +0000

    add constructer in registercontroller

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+}
